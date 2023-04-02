4e227083398e13983870abef19e61be010d1a2db
Santiago Pena Martinez <spenamartinez@km3net.de>
2023-04-02 12:22:54 +0200

    Fix test according to import module

--- HPO/test_tune_module.py
+++ HPO/test_tune_module.py
@@ -1,34 +1,12 @@
-import h5py
 import matplotlib.pyplot as plt
-import tensorflow as tf
-import pandas as pd
-import tune_module
-import numpy as np
-import pandas as pd
-import simpler_tune_module as stm
 import optuna_tuner as ot
+import sys
+# caution: path[0] is reserved for script path (or '' in REPL)
+sys.path.insert(1, '../src/dnn_energy_estimate/training/')
+from data_set import get_dataset
 
-
-def get_dataset(fpath: str):
-    with h5py.File(fpath + "/for_train.h5", "r") as f:
-        with h5py.File(fpath + "/for_val.h5", "r") as g:
-            print("Opening hdf5...")
-            X_train_scaled = tf.convert_to_tensor(pd.DataFrame(f["X"]['JSHOWERFIT_ENERGY',
-                                                                          'JENERGY_ENERGY', 'lik_first_JENERGY',
-                                                                          'lik_first_JSHOWERFIT', 'trig_hits', 'trig_doms', 'trig_lines', 'JSTART_LENGTH_METRES', 'dir_x_gandalf', 'dir_y_gandalf', 'dir_z_gandalf','dir_x_showerfit', 'dir_y_showerfit', 'dir_z_showerfit']).to_numpy())
-            X_val_scaled = tf.convert_to_tensor(pd.DataFrame(g["X"]['JSHOWERFIT_ENERGY',
-                                                                          'JENERGY_ENERGY', 'lik_first_JENERGY',
-                                                                          'lik_first_JSHOWERFIT', 'trig_hits', 'trig_doms', 'trig_lines', 'JSTART_LENGTH_METRES', 'dir_x_gandalf', 'dir_y_gandalf', 'dir_z_gandalf','dir_x_showerfit', 'dir_y_showerfit', 'dir_z_showerfit']).to_numpy())
-            y_train = np.asarray(f["y"])
-            X_train_weights = np.asarray(f["weights"])
-            y_val = np.asarray(g["y"])
-            X_val_weights = np.asarray(g["weights"])
-            print("finished opening hdf5...")
-    return X_train_scaled, X_val_scaled, y_train, X_train_weights, y_val, X_val_weights
 
 X_train_scaled, X_val_scaled, y_train, X_train_weights, y_val, X_val_weights = get_dataset("../../")
-
-
 study_name = "tune_test"
 study = ot.HPO(X_train_scaled, y_train, X_val_scaled, y_val, X_train_weights, X_val_weights, study_name = study_name)
 print(study.trials_dataframe().sort_values(["value"]).head())
