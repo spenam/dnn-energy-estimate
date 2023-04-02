fd50e97c6f03116d6af3fb101c813d2ad6e06e6d
Santiago Pena Martinez <spenamartinez@km3net.de>
2023-04-02 12:22:36 +0200

    Add module to import dataset

---
+++ src/dnn_energy_estimate/training/data_set.py
@@ -0,0 +1,21 @@
+import h5py
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+
+def get_dataset(fpath: str):
+    with h5py.File(fpath + "/for_train.h5", "r") as f:
+        with h5py.File(fpath + "/for_val.h5", "r") as g:
+            print("Opening hdf5...")
+            X_train_scaled = tf.convert_to_tensor(pd.DataFrame(f["X"]['JSHOWERFIT_ENERGY',
+                                                                          'JENERGY_ENERGY', 'lik_first_JENERGY',
+                                                                          'lik_first_JSHOWERFIT', 'trig_hits', 'trig_doms', 'trig_lines', 'JSTART_LENGTH_METRES', 'dir_x_gandalf', 'dir_y_gandalf', 'dir_z_gandalf','dir_x_showerfit', 'dir_y_showerfit', 'dir_z_showerfit']).to_numpy())
+            X_val_scaled = tf.convert_to_tensor(pd.DataFrame(g["X"]['JSHOWERFIT_ENERGY',
+                                                                          'JENERGY_ENERGY', 'lik_first_JENERGY',
+                                                                          'lik_first_JSHOWERFIT', 'trig_hits', 'trig_doms', 'trig_lines', 'JSTART_LENGTH_METRES', 'dir_x_gandalf', 'dir_y_gandalf', 'dir_z_gandalf','dir_x_showerfit', 'dir_y_showerfit', 'dir_z_showerfit']).to_numpy())
+            y_train = np.asarray(f["y"])
+            X_train_weights = np.asarray(f["weights"])
+            y_val = np.asarray(g["y"])
+            X_val_weights = np.asarray(g["weights"])
+            print("finished opening hdf5...")
+    return X_train_scaled, X_val_scaled, y_train, X_train_weights, y_val, X_val_weights
