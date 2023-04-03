489fe5f3d6af5ad1d5092672a15ee9fbabedf36a
Santiago Pena Martinez <spenamartinez@km3net.de>
2023-04-03 16:58:39 +0200

    Important, add list feature to data load
    Now it loads features taken from a list.

--- src/dnn_energy_estimate/training/data_set.py
+++ src/dnn_energy_estimate/training/data_set.py
@@ -3,16 +3,18 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 
-def get_dataset(fpath: str):
+def get_dataset(fpath: str, features):
     with h5py.File(fpath + "/for_train.h5", "r") as f:
         with h5py.File(fpath + "/for_val.h5", "r") as g:
             print("Opening hdf5...")
-            X_train_scaled = tf.convert_to_tensor(pd.DataFrame(f["X"]['JSHOWERFIT_ENERGY',
-                                                                          'JENERGY_ENERGY', 'lik_first_JENERGY',
-                                                                          'lik_first_JSHOWERFIT', 'trig_hits', 'trig_doms', 'trig_lines', 'JSTART_LENGTH_METRES', 'dir_x_gandalf', 'dir_y_gandalf', 'dir_z_gandalf','dir_x_showerfit', 'dir_y_showerfit', 'dir_z_showerfit']).to_numpy())
-            X_val_scaled = tf.convert_to_tensor(pd.DataFrame(g["X"]['JSHOWERFIT_ENERGY',
-                                                                          'JENERGY_ENERGY', 'lik_first_JENERGY',
-                                                                          'lik_first_JSHOWERFIT', 'trig_hits', 'trig_doms', 'trig_lines', 'JSTART_LENGTH_METRES', 'dir_x_gandalf', 'dir_y_gandalf', 'dir_z_gandalf','dir_x_showerfit', 'dir_y_showerfit', 'dir_z_showerfit']).to_numpy())
+            #X_train_scaled = tf.convert_to_tensor(pd.DataFrame(f["X"]['JSHOWERFIT_ENERGY',
+            #                                                              'JENERGY_ENERGY', 'lik_first_JENERGY',
+            #                                                              'lik_first_JSHOWERFIT', 'trig_hits', 'trig_doms', 'trig_lines', 'JSTART_LENGTH_METRES', 'dir_x_gandalf', 'dir_y_gandalf', 'dir_z_gandalf','dir_x_showerfit', 'dir_y_showerfit', 'dir_z_showerfit']).to_numpy())
+            #X_val_scaled = tf.convert_to_tensor(pd.DataFrame(g["X"]['JSHOWERFIT_ENERGY',
+            #                                                              'JENERGY_ENERGY', 'lik_first_JENERGY',
+            #                                                              'lik_first_JSHOWERFIT', 'trig_hits', 'trig_doms', 'trig_lines', 'JSTART_LENGTH_METRES', 'dir_x_gandalf', 'dir_y_gandalf', 'dir_z_gandalf','dir_x_showerfit', 'dir_y_showerfit', 'dir_z_showerfit']).to_numpy())
+            X_train_scaled = tf.convert_to_tensor(pd.DataFrame({key: f["X"][key][:] for key in features}).to_numpy())
+            X_val_scaled = tf.convert_to_tensor(pd.DataFrame({key: g["X"][key][:] for key in features}).to_numpy())
             y_train = np.asarray(f["y"])
             X_train_weights = np.asarray(f["weights"])
             y_val = np.asarray(g["y"])
